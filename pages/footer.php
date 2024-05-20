c411846f395ffe18e32afe53354671e52be5f7eb
PhamCongNhutTan <nhuttan2607pm@gmail.com>
2024-05-20 19:14:19 +0700

    refactor: thư mục

---
+++ pages/footer.php
@@ -0,0 +1,33 @@
+<!-- FOOTER -->
+<div id="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-4">
+                <h6>LIÊN HỆ VỚI CHÚNG TÔI</h6>
+                <ul>
+                    <li><a href="" target="_blank"><i class="fa-solid fa-phone"></i> Phone: +84012345</a></li>
+                    <li><a href="" target="_blank"><i class="fa-solid fa-envelope"></i> Email: 52200000@student.tdtu.edu.vn</a></li>
+                    <li><a href="" target="_blank"><i class="fa-solid fa-location-dot"></i> Address: 1234, TP.HCM</a></li>
+                </ul>
+            </div>
+            <div class="col-12 col-md-4">
+                <h6>KẾT NỐI VỚI CHÚNG TÔI</h6>
+                <ul>
+                    <li class="social-icon"><a href="" target="_blank"><i class="fa-brands fa-facebook"></i></a></li>
+                    <li class="social-icon"><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank"><i class="fa-brands fa-youtube"></i></a></li>
+                    <li class="social-icon"><a href="" target="_blank"><i class="fa-brands fa-instagram"></i></a></li>
+                    <li class="social-icon"><a href="" target="_blank"><i class="fa-brands fa-tiktok"></i></a></li>
+                </ul>
+            </div>
+            <div class="col-12 col-md-4">
+                <h6>LIÊN KẾT NHANH</h6>
+                <ul>
+                    <li><a href="" target="_blank">Chuyến đi</a></li>
+                    <li><a href="" target="_blank">Tin tức</a></li>
+                    <li><a href="" target="_blank">Liện hệ</a></li>
+                    <li><a href="" target="_blank">Giỏ hàng</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ pages/main.php
@@ -0,0 +1,22 @@
+<!-- MAIN -->
+<div id="main">
+    <?php
+        if(isset($_GET['quanly'])) {
+            $temp = $_GET['quanly'];
+        } else {
+            $temp = '';
+        }
+
+        if ($temp == 'chuyendi') {
+            include('main_content/chuyendi.php');
+        } else if ($temp == 'tintuc') {
+            include('main_content/tintuc.php');
+        } else if ($temp == 'lienhe') {
+            include('main_content/lienhe.php');
+        } else if ($temp == 'giohang') {
+            include('main_content/giohang.php');
+        } else {
+            include('main_content/trangchu.php');
+        }
+    ?>
+</div>
