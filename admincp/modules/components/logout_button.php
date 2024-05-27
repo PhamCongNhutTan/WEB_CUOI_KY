b6132c322a9069149b90230ccc3ceae08504839f
nauteik <132426238+nauteik@users.noreply.github.com>
2024-05-27 09:07:42 +0700

    fix: admincp không có logout

--- admincp/modules/components/logout_button.php
+++ admincp/modules/components/logout_button.php
@@ -2,7 +2,7 @@
     <a id="admin" style="text-decoration: none; font-weight: 500;" href="../">Trang chủ</a>
 </button>
 <form action="index.php" method="GET">
-    <button type="submit" value="1" name="dangxuat" class="logout btn btn-dark border-0 me-3 px-1" style="background-color:inherit; font-size:22px; border-radius:3rem;">Đăng xuất</button>
+    <button type="submit" value="1" name="dangxuat" class="logout btn btn-dark border-0 me-3 px-1" style="font-size:22px; border-radius:3rem;">Đăng xuất</button>
 </form>
 <img class="border-3 rounded-circle mx-4" style="object-fit: cover; width: 30px; height: 30px;" src=<?php if (isset($user)) if ($user->getImagePath() == null) echo "../images/avatar/default.png";
                                                                                                     else echo "../images/avatar/" . $user->getImagePath(); ?>>
