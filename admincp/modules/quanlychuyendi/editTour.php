8336bcd3a93c6fbd8717f495a8125b2284462b80
nauteik <132426238+nauteik@users.noreply.github.com>
2024-05-29 02:07:45 +0700

    feat: quản lý chi tiết chuyến đi ( chưa có xem phía user )

--- admincp/modules/quanlychuyendi/editTour.php
+++ admincp/modules/quanlychuyendi/editTour.php
@@ -3,102 +3,127 @@ $tourID = $_GET["tour_id"];
 $tour = TourController::getTourByID($tourID);
 ?>
 <div class="container" id="tour">
-  <h3 class="mt-3"><span class="bar"></span>Cập nhật chuyến đi</h3>
-  <form id="editTourForm" method="POST" action="modules/quanlychuyendi/execute.php" enctype="multipart/form-data">
-    <input type="text" class="d-none" name="tourID" value="<?php echo $tourID ?>">
-    <div class="row">
-      <div class="col-12 col-lg-4">
-        <div class="form-group mt-3">
-          <label for="">Tên chuyến đi</label>
-          <input type="text" class="form-control" id="" name="name" value="<?php echo $tour->getName() ?>">
+    <div id="alert" class="alert">
+         <span class="msg">Cập nhật chuyến đi thành công</span>
+         <div class="close-btn">
+            <span class="fas fa-times"></span>
+         </div>
+    </div>
+    <h3 style="text-align: left;" class="mt-3 "><span class="bar"></span>Cập nhật chuyến đi</h3>
+    <form id="editTourForm" method="POST" action="modules/quanlychuyendi/execute.php" enctype="multipart/form-data">
+        <input type="text" class="d-none" name="tourID" value="<?php echo $tourID ?>">
+        <div class="row">
+            <div class="col-12 col-lg-4">
+                <div class="form-group mt-3">
+                    <label for="">Tên chuyến đi</label>
+                    <input type="text" class="form-control" id="" name="name" value="<?php echo $tour->getName() ?>">
+                </div>
+            </div>
+            <div class="col-12 col-lg-4">
+                <div class="form-group mt-3">
+                    <label for="">Chi phí</label>
+                    <input type="text" class="form-control" name="basePrice" value="<?php echo number_format($tour->getBasePrice()) ?>" oninput="formatPrice(this)">
+                </div>
+            </div>
+            <div class="col-12 col-lg-4">
+                <div class="form-group mt-3">
+                    <label style="display:block;" for="">Trạng thái</label>
+                    <select class="form-select" style="width: 100%; padding: 4px;" name="type" id="">
+                        <?php
+                        if ($tour->getType() == "Đang mở") {
+                        ?>
+                            <option value="Đang mở" selected>Đang mở</option>
+                            <option value="Đang đóng">Đang đóng</option>
+                        <?php
+                        } else {
+                        ?>
+                            <option value="Đang đóng">Đang đóng</option>
+                            <option value="Đang mở" selected>Đang mở</option>
+                        <?php
+                        }
+                        ?>
+                    </select>
+                </div>
+            </div>
         </div>
-      </div>
-      <div class="col-12 col-lg-4">
         <div class="form-group mt-3">
-          <label for="">Chi phí</label>
-          <input type="number" class="form-control" name="basePrice" value="<?php echo $tour->getBasePrice() ?>">
+            <label for="">Địa điểm</label>
+            <input type="text" class="form-control" id="" name="location" value="<?php echo $tour->getLocation() ?>">
         </div>
-      </div>
-      <div class="col-12 col-lg-4">
         <div class="form-group mt-3">
-          <label style="display:block;" for="">Trạng thái</label>
-          <select class="form-select" style="width: 100%; padding: 4px;" name="type" id="">
-            <?php
-            if ($tour->getType() == "Đang mở") {
-              ?>
-              <option value="Đang mở" selected>Đang mở</option>
-              <option value="Đang đóng">Đang đóng</option>
-              <?php
-            } else {
-              ?>
-              <option value="Đang đóng">Đang đóng</option>
-              <option value="Đang mở" selected>Đang mở</option>
-              <?php
-            }
-            ?>
-          </select>
+            <label for="">Mô tả</label>
+            <textarea rows="2" class="form-control" id="description" name="description"><?php echo $tour->getDescription() ?></textarea>
         </div>
-      </div>
-    </div>
-    <div class="form-group mt-3">
-      <label for="">Địa điểm</label>
-      <input type="text" class="form-control" id="" name="location" value="<?php echo $tour->getLocation() ?>">
-    </div>
-    <div class="form-group mt-3">
-      <label for="">Mô tả</label>
-      <textarea style="resize: none;" rows="10" class="form-control" id="description"
-        name="description"><?php echo $tour->getDescription() ?></textarea>
-    </div>
-    <div class="form-group mt-3">
-      <label for="">Hình ảnh</label>
-      <input style="border-bottom-left-radius: 0px ;border-bottom-right-radius: 0px;" type="file" class="form-control"
-        id="imagePath" name="imagePath"
-        src="<?php echo "../images/tour/" . $tour->getTourID() . "/" . $tour->getImagePath() ?>">
-      <img id="currentImage" style="width: 100%; max-width: 100%;" class="border-3 border-top-0 rounded-bottom"
-        src="<?php echo "../images/tour/" . $tour->getTourID() . "/" . $tour->getImagePath() ?>" width="100px" alt="">
-    </div>
-    <div id="notification" class="mt-3 alert alert-success alert-dismissible d-none" role="alert">Cập nhật chuyến đi
-      thành công</div>
-    <button id="editTourBtn" type="submit" name="editTour" class="btn btn-success mt-3">Cập nhật chuyến đi</button>
-  </form>
+        <div class="form-group mt-3">
+            <label for="">Chi tiết chuyến đi</label>
+            <textarea style="resize: none;" rows="10" class="form-control" id="detail" name="detail"><?php echo $tour->getDetail() ?></textarea>
+        </div>
+        <div class="form-group mt-3">
+            <label for="">Hình ảnh</label>
+            <input style="border-bottom-left-radius: 0px ;border-bottom-right-radius: 0px;" type="file" class="form-control" id="imagePath" name="imagePath" src="<?php echo "../images/tour/" . $tour->getTourID() . "/" . $tour->getImagePath() ?>">
+            <img id="currentImage" style="width: 100%; max-width: 500px; margin: 0px auto;" class="border-3 border-top-0 rounded-bottomd d-block" src="<?php echo "../images/tour/" . $tour->getTourID() . "/" . $tour->getImagePath() ?>" width="100px" alt="">
+        </div>
+        <button id="editTourBtn" type="submit" name="editTour" class="btn btn-success mt-3">Cập nhật chuyến đi</button>
+    </form>
 </div>
 <script>
-  document.addEventListener("DOMContentLoaded", function () {
-    const fileInput = document.getElementById("imagePath");
-    const preview = document.getElementById("currentImage");
-    fileInput.addEventListener("change", function () {
-      const file = fileInput.files[0];
-      if (file) {
-        const reader = new FileReader();
-        reader.onload = function (e) {
-          preview.src = e.target.result; // Display the selected image
-        }
-        reader.readAsDataURL(file); // Convert the file to a data URL
-      }
+    function formatPrice(input) {
+        let value = input.value.replace(/\D/g, ''); // Remove all non-digit characters
+        let formattedValue = value.replace(/\B(?=(\d{3})+(?!\d))/g, ','); // Add commas
+        input.value = formattedValue;
+    }
+    function alertSuccess(){
+        $('.alert').addClass("show");
+        $('.alert').removeClass("hide");
+        $('.alert').addClass("showAlert");
+        setTimeout(function() {
+            $('.alert').removeClass("show");
+            $('.alert').addClass("hide");
+        }, 3000);
+    }
+    $('.close-btn').click(function() {
+        $('.alert').removeClass("show");
+        $('.alert').addClass("hide");
     });
-    const form = document.getElementById("editTourForm");
-    form.addEventListener("submit", function (e) {
-      e.preventDefault(); // Prevent default form submission
-      var formData = new FormData(this);
-      formData.append("editTour", "true");
-      var xhr = new XMLHttpRequest();
-      xhr.open("POST", "modules/quanlychuyendi/execute.php", true);
-      xhr.onload = function () {
-        if (xhr.status === 200) {
-          // Success handling
-          document.getElementById("notification").classList.remove("d-none");
-          setTimeout(function () {
-            document.getElementById("notification").classList.add("d-none");
-          }, 3000); // Hide notification after 3 seconds
-        } else {
-          // Error handling
-          console.error(xhr.responseText);
-        }
-      };
-      xhr.onerror = function () {
-        console.error("Request failed");
-      };
-      xhr.send(formData);
+    document.addEventListener("DOMContentLoaded", function() {
+        const fileInput = document.getElementById("imagePath");
+        const preview = document.getElementById("currentImage");
+        fileInput.addEventListener("change", function() {
+            const file = fileInput.files[0];
+            if (file) {
+                const reader = new FileReader();
+                reader.onload = function(e) {
+                    preview.src = e.target.result; // Display the selected image
+                }
+                reader.readAsDataURL(file); // Convert the file to a data URL
+            }
+        });
+        preview.addEventListener('click', function() {
+            file.click(); // Click on the file input element
+        });
+        const form = document.getElementById("editTourForm");
+        form.addEventListener("submit", function(e) {
+            e.preventDefault(); // Prevent default form submission
+            var formData = new FormData(this);
+            formData.append("editTour", "true");
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", "modules/quanlychuyendi/execute.php", true);
+            xhr.onload = function() {
+                if (xhr.status === 200) {
+                    // Success handling
+                    alertSuccess();
+                    setTimeout(function() {
+                        document.getElementById("notification").classList.add("d-none");
+                    }, 3000); // Hide notification after 3 seconds
+                } else {
+                    // Error handling
+                    console.error(xhr.responseText);
+                }
+            };
+            xhr.onerror = function() {
+                console.error("Request failed");
+            };
+            xhr.send(formData);
+        });
     });
-  });
 </script>
--- admincp/modules/quanlychuyendi/viewTour.php
+++ admincp/modules/quanlychuyendi/viewTour.php
@@ -1,6 +1,6 @@
 <?php
-include_once ("../enities/tour.class.php");
-include_once ("../modules/tour_controller.php");
+include_once("../enities/tour.class.php");
+include_once("../modules/tour_controller.php");
 if (isset($_GET["type"])) {
     $type = $_GET["type"];
 } else {
@@ -8,39 +8,38 @@ if (isset($_GET["type"])) {
 }
 ?>
 <div class="container" id="viewTour">
+    <h3 style="text-align: left;" class="mt-3 "><span class="bar"></span>Quản lý chuyến đi</h3>
     <a style="text-decoration: none;" href="?action=quanlychuyendi&query=add">
-        <button id="create" type="button" class="btn btn-success text-success ms-2 mt-3"
-            style="background-color: inherit">
-            <i class="bi bi-plus-circle text-success"></i><a style="text-decoration: none; color: green;"
-                href="?action=quanlychuyendi&query=add"> Thêm chuyến đi</a>
+        <button id="create" type="button" class="btn btn-success text-success ms-2 mt-3" style="background-color: inherit">
+            <i class="bi bi-plus-circle text-success"></i><a style="text-decoration: none; color: green;" href="?action=quanlychuyendi&query=add"> Thêm chuyến đi</a>
         </button>
     </a>
     <div class="mt-3">
         <label style="display:block;" for="">Trạng thái</label>
-        <select class="form-select" style="width: 25%; padding: 4px;" name="type" id="" onchange="handleSelect(this);">
+        <select class="form-select rounded-3" style="width: 25%; padding: 4px;" name="type" id="" onchange="handleSelect(this);">
             <option value="Không" <?php if ($type == "khong" || $type == "")
-                echo "selected" ?>>Không</option>
-                <option value="Đang mở" <?php if ($type == "dangmo")
-                echo "selected" ?>>Đang mở</option>
-                <option value="Đang đóng" <?php if ($type == "dangdong")
-                echo "selected" ?>>Đang đóng</option>
-            </select>
-        </div>
-        <table id="tour" class="table table-md table-striped my-3">
-            <thead>
-                <tr>
-                    <th scope="col">Thứ tự</th>
-                    <th scope="col">Tên chuyến đi</th>
-                    <th scope="col">Địa điểm</th>
-                    <th scope="col">Mô tả</th>
-                    <th scope="col">Chi phí</th>
-                    <th scope="col">Hình ảnh</th>
-                    <th scope="col">Tình trạng</th>
-                    <th scope="col"></th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
+                                        echo "selected" ?>>Không</option>
+            <option value="Đang mở" <?php if ($type == "dangmo")
+                                        echo "selected" ?>>Đang mở</option>
+            <option value="Đang đóng" <?php if ($type == "dangdong")
+                                            echo "selected" ?>>Đang đóng</option>
+        </select>
+    </div>
+    <table id="tour" class="table table-md table-striped my-3">
+        <thead>
+            <tr>
+                <th scope="col">Thứ tự</th>
+                <th scope="col">Tên chuyến đi</th>
+                <th scope="col">Địa điểm</th>
+                <th scope="col">Mô tả</th>
+                <th scope="col">Chi phí</th>
+                <th scope="col">Hình ảnh</th>
+                <th scope="col">Tình trạng</th>
+                <th scope="col"></th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
 
 
             $tourList = TourController::getTours();
@@ -59,40 +58,37 @@ if (isset($_GET["type"])) {
                 }
             }
             foreach ($filteredTourList as $tour) {
-                ?>
+            ?>
                 <tr class="py-3">
                     <td style="text-align: center;"><?php echo $tour->getTourID() ?></td>
                     <td><?php echo $tour->getName() ?></td>
                     <td><?php echo $tour->getLocation() ?></td>
-                    <td style="text-wrap: nowrap;"><?php echo $tour->getDescription() ?></td>
-                    <td><?php echo $tour->getBasePrice() ?> VND</td>
+                    <td><?php echo $tour->getDescription() ?></td>
+                    <td><?php echo $tour->getBasePrice() ?></td>
                     <td>
-                        <img style="width: 100%; max-width: 300px;" class="border-2 rounded"
-                            src="<?php echo "../images/tour/" . $tour->getTourID() . "/" . $tour->getImagePath() ?>"
-                            width="100px" alt="">
+                        <img style="width: 100%; max-width: 300px;" class="border-2 rounded" src="<?php echo "../images/tour/" . $tour->getTourID() . "/" . $tour->getImagePath() ?>" width="100px" alt="">
                     </td>
                     <td style="text-wrap: nowrap;"><?php echo ($tour->getType() ?? "Không có") ?></td>
                     <td>
-                        <button type="button" class="btn btn-primary"> <a style="text-decoration:none; color:white;"
-                                href="?action=quanlychuyendi&query=edit&tour_id=<?php echo $tour->getTourID() ?>"><i
-                                    class="bi bi-pencil"></i></a></button>
+                        <button type="button" class="btn btn-primary"> <a style="text-decoration:none; color:white;" href="?action=quanlychuyendi&query=edit&tour_id=<?php echo $tour->getTourID() ?>"><i class="bi bi-pencil"></i></a></button>
                     </td>
                 </tr>
-                <?php
+            <?php
             }
             ?>
         </tbody>
     </table>
 </div>
 <script>
+    
     function handleSelect(select) {
-        var selectedValue = select.value;
-        if (selectedValue === "Không") {
-            location.href = "?action=quanlychuyendi&query=view&type=khong";
-        } else if (selectedValue === "Đang mở") {
-            location.href = "?action=quanlychuyendi&query=view&type=dangmo";
-        } else if (selectedValue === "Đang đóng") {
-            location.href = "?action=quanlychuyendi&query=view&type=dangdong";
+            var selectedValue = select.value;
+            if (selectedValue === "Không") {
+                location.href = "?action=quanlychuyendi&query=view&type=khong";
+            } else if (selectedValue === "Đang mở") {
+                location.href = "?action=quanlychuyendi&query=view&type=dangmo";
+            } else if (selectedValue === "Đang đóng") {
+                location.href = "?action=quanlychuyendi&query=view&type=dangdong";
+            }
         }
-    }
 </script>
